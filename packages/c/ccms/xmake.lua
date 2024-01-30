20898e82c1776e6594151f7d115e209e1cd81864
Hendrik Boeck <hendrikboeck.dev@protonmail.com>
2024-01-30 18:22:21 +0100

    Added ccms library

---
+++ packages/c/ccms/xmake.lua
@@ -0,0 +1,9 @@
+package("ccms")
+  set_description("ccms: collection of C memory structures")
+
+  add_urls("https://github.com/hendrikboeck/ccms.git")
+  add_versions("0.1.0", "c41140665824f3eae98b4f6a6546898b72379ec9")
+
+  on_install(function (package)
+    import("package.tools.xmake").install(package)
+  end)
--- packages/m/micro_sockets/xmake.lua
+++ packages/m/micro_sockets/xmake.lua
@@ -1,7 +1,7 @@
 package("micro_sockets")
-  set_description("MICRO_SOCKETS: lilbrary to simplify C sockets")
+  set_description("µ-sockets: library to simplify C TCP/UDP sockets")
 
-  add_urls("https://github.com/hendrikboeck/micro_sockets.git")
+  add_urls("https://github.com/hendrikboeck/micro-sockets.git")
   add_versions("0.1.0", "d88041b827c3e51a2a7bea0047ddd613c27be676")
 
   on_install(function (package)
