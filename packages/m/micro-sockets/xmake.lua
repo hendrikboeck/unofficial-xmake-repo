467c008289ac6d1cbd5898a0f4e958efdc05a333
Hendrik Boeck <hendrikboeck.dev@protonmail.com>
2024-01-30 18:29:09 +0100

    Added ccms as dependency to micro-sockets

--- packages/m/micro-sockets/xmake.lua
+++ packages/m/micro-sockets/xmake.lua
@@ -1,8 +1,10 @@
-package("micro_sockets")
+package("micro-sockets")
   set_description("µ-sockets: library to simplify C TCP/UDP sockets")
 
   add_urls("https://github.com/hendrikboeck/micro-sockets.git")
   add_versions("0.1.0", "d88041b827c3e51a2a7bea0047ddd613c27be676")
+
+  add_deps("ccms")
 
   on_install(function (package)
     import("package.tools.xmake").install(package)
