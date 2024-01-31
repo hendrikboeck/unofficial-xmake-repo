c604e10a9c89ca4bdc61d60de78b39b33ae6df03
Hendrik Boeck <hendrikboeck.dev@protonmail.com>
2024-01-31 14:57:26 +0100

    just use latest version git

--- packages/m/micro-sockets/xmake.lua
+++ packages/m/micro-sockets/xmake.lua
@@ -2,7 +2,6 @@ package("micro-sockets")
   set_description("µ-sockets: library to simplify C TCP/UDP sockets")
 
   add_urls("https://github.com/hendrikboeck/micro-sockets.git")
-  add_versions("@dev", "main")
   -- add_versions("0.1.0", "628eb438427ea2577b94b51e1eefb6ba4c003d78")
 
   add_deps("ccms")
